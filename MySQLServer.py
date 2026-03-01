ae8fabb1a6c88bad6c245ee94a26c9f82384667b
Brunokayitar <brunokayitare85@gmail.com>
2026-03-01 05:07:26 +0200

    Create MySQLServer.py script to safely create alx_book_store database

--- MySQLServer.py
+++ MySQLServer.py
@@ -2,21 +2,23 @@ import mysql.connector
 from mysql.connector import Error
 
 try:
-    # Establish the connection
+    
     connection = mysql.connector.connect(
         host="localhost",
         user="root",
-        password="",       # your password if any
-        database="your_db_name"  # replace with your database
+        password=""  
     )
 
     if connection.is_connected():
-        print("Connected to MySQL database")
+        cursor = connection.cursor()
+        cursor.execute("CREATE DATABASE IF NOT EXISTS alxbookstore")
+        print("Database 'alxbookstore' created successfully!")
 
 except Error as e:
     print(f"Error while connecting to MySQL: {e}")
 
 finally:
     if 'connection' in locals() and connection.is_connected():
+        cursor.close()
         connection.close()
         print("MySQL connection is closed")
